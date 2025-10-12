32f5d4dc9c8e12e1ed6a2594a7fe462525739f63
anshu706 <anshupadhayay212@gmail.com>
2025-10-12 16:15:24 +0530

    butterfly pattern

---
+++ Ch-4_Patterns/hollow-diamond_butterfly_pattern.cpp
@@ -0,0 +1,134 @@
+/*
+HOLLOW DIAMOND PATTERN
+
+     *
+   *   *
+  *     *
+ *       *
+  *     *
+   *   *
+     *
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n = 4;
+
+  // top
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      cout << " ";
+    }
+    cout << "*";
+
+    if (i != 0)
+    {
+      // spaces
+      for (int j = 0; j < 2 * i - 1; j++)
+      {
+        cout << " ";
+      }
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  // bottom
+
+  for (int i = 0; i < n - 1; i++)
+  {
+    // spaces
+    for (int j = 0; j < i + 1; j++)
+    {
+      cout << " ";
+    }
+
+    cout << "*";
+
+    if (i != n - 2)
+    {
+      // spaces
+      for (int j = 0; j < 2 * (n - i) - 5; j++)
+      {
+        cout << " ";
+      }
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
+*/
+
+//--------------------------------------------------------------------------------------------------------------------------
+
+/* BUTTERFLY PATTERN
+*      *
+**    **
+***  ***
+********
+********
+***  ***
+**    **
+*      *
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n = 4;
+
+  // Upper half
+  for (int i = 1; i <= n; i++)
+  {
+    // Left stars
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    // Spaces
+    for (int j = 1; j <= 2 * (n - i); j++)
+    {
+      cout << " ";
+    }
+    // Right stars
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  // Lower half
+  for (int i = n; i >= 1; i--)
+  {
+    // Left stars
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    // Spaces
+    for (int j = 1; j <= 2 * (n - i); j++)
+    {
+      cout << " ";
+    }
+    // Right stars
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
+*/
