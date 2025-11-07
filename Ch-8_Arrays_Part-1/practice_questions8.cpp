511482a0cc03301ab647954e9182902e816e41aa
anshu706 <anshupadhayay212@gmail.com>
2025-11-07 22:00:29 +0530

    Ch-8 arrays

---
+++ Ch-8_Arrays_Part-1/practice_questions8.cpp
@@ -0,0 +1,114 @@
+// FIND SMALLEST/LARGEST IN ARRAY
+/*
+#include <iostream>
+#include <climits>
+
+using namespace std;
+int main()
+{
+    int nums[] = {5, 15, 22, 1, -15, 24};
+    int size = 6;
+
+    int smallest = INT_MAX;
+    int largest = INT_MIN;
+
+    for (int i = 0; i < size; i++)
+    {
+        smallest = min(nums[i], smallest);
+        largest = max(nums[i], largest);
+    }
+
+    cout << "Smallest = " << smallest << endl;
+    cout << "Largest = " << largest << endl;
+
+    return 0;
+}
+*/
+
+// --------------------------------------------------------------------------------------------------------------
+
+//  REVERSE AN ARRAY  [Two pointer approach]
+
+/*
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size)
+{
+    int start = 0, end = size - 1;
+
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    int arr[] = {4, 2, 7, 8, 1, 2, 3};
+    int size = 7;
+
+    reverseArray(arr, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+*/
+
+// WAF TO CALCULATE SUM & PRODUCT OF ALL NUMBERS IN AN ARRAY
+
+/*
+#include <iostream>
+using namespace std;
+
+int Sum(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int Product(int arr[], int n)
+{
+    int product = 1;
+    for (int i = 0; i < n; i++)
+    {
+        product *= arr[i];
+    }
+    return product;
+}
+
+int main()
+{
+    int arr[] = {4, 3, 5, 6, 9, 1, 2};
+    int n = 7;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    int sum = Sum(arr, n);
+    int product = Product(arr, n);
+
+    cout << "\nSum of all elements = " << sum << endl;
+    cout << "Product of all elements = " << product << endl;
+
+    return 0;
+}
+*/
+
+// WAF TO SWAP THE MAX & MIN NUMBER OF AN ARRAY
+
+// WAF TO PRINT ALL THE UNIQUE VALUES IN AN ARRAY.
+
+// WAF TO PRINT INTERSECTION OF 2 ARRAYS.
