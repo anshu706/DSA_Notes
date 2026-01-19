8b8d4002d951e6b5b0f66e29a85b7a2f97d25ddf
anshu706 <anshupadhayay212@gmail.com>
2026-01-19 19:23:41 +0530

    new

--- Leetcode/majority_element_169.cpp
+++ Leetcode/majority_element_169.cpp
@@ -1,14 +1,13 @@
 // MAJORITY ELEMENT 169
 
-// Brute Force Approach
+// Brute Force Approach --------------------------------------------------------------------------------------------------
+// O(n^2)
 
-#include <iostream>
-#include <vector>
-
+/*
 class Solution
 {
 public:
-    int majorityElement(vector<int> &nums)
+int majorityElement(vector<int> &nums)
     {
         int n = nums.size();
 
@@ -31,3 +30,73 @@ public:
         return -1;
     }
 };
+*/
+
+// Optimized Approach - [sorting] --------------------------------------------------------------------------------------------------
+// O(nlogn)
+
+/*
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        // sort
+        sort(nums.begin(), nums.end());
+
+        // freq count
+        int freq = 1, ans = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                freq++;
+            }
+            else
+            {
+                freq = 1;
+                ans = nums[i];
+            }
+
+            if (freq > n / 2)
+            {
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
+*/
+
+// Moore's Voting Algorithm --------------------------------------------------------------------------------------------------
+// O(n)
+
+/*
+class Solution
+{
+    public:
+    int majorityElement(vector<int> &nums)
+    {
+        int freq = 0, ans = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (freq == 0)
+            {
+                ans = nums[i];
+            }
+            if (ans == nums[i])
+            {
+                freq++;
+            }
+            else
+            {
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
+*/
